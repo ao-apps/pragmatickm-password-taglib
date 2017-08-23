f9a74df2cf8b1c5776fb0de711178c69dd438e4e
AO Industries, Inc <support@aoindustries.com>
2017-08-23 16:25:54 -0500

    CurrentCaptureLevel moved to new semanticcms-core-pages-local module

--- src/main/java/com/pragmatickm/password/taglib/CustomFieldTag.java
+++ src/main/java/com/pragmatickm/password/taglib/CustomFieldTag.java
@@ -31,7 +31,7 @@ import com.pragmatickm.password.model.Password;
 import com.semanticcms.core.model.Node;
 import com.semanticcms.core.model.PageRef;
 import com.semanticcms.core.pages.CaptureLevel;
-import com.semanticcms.core.servlet.CurrentCaptureLevel;
+import com.semanticcms.core.pages.local.CurrentCaptureLevel;
 import com.semanticcms.core.servlet.CurrentNode;
 import com.semanticcms.core.servlet.PageRefResolver;
 import java.io.IOException;
