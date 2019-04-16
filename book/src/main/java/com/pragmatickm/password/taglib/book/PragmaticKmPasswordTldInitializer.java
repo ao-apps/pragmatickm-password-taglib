360aeea3e53ba7fd721b79d9b10e47a241bb6a88
AO Industries, Inc <support@aoindustries.com>
2019-04-15 20:51:48 -0500

    Using Maven property "documented.javac.link.javaApi" to link to the correct Java API version

--- book/src/main/java/com/pragmatickm/password/taglib/book/PragmaticKmPasswordTldInitializer.java
+++ book/src/main/java/com/pragmatickm/password/taglib/book/PragmaticKmPasswordTldInitializer.java
@@ -33,7 +33,7 @@ public class PragmaticKmPasswordTldInitializer extends TagReferenceInitializer {
 			"Taglib Reference",
 			"/password/taglib",
 			"/pragmatickm-password.tld",
-			Maven.properties.getProperty("javac.link.javaApi.jdk7"),
+			Maven.properties.getProperty("documented.javac.link.javaApi"),
 			Maven.properties.getProperty("javac.link.javaeeApi.6"),
 			Collections.singletonMap("com.pragmatickm.password.taglib.", Maven.properties.getProperty("documented.url") + "apidocs/")
 		);
