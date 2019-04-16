e70632c8bcb1908cc48e13493e99322a6d776d2e
AO Industries, Inc <support@aoindustries.com>
2019-04-15 19:34:23 -0500

    Fixed cross-project linking of javadocs (really this time).
    The issue was our redirects from */apidocs/ to */apidocs/index.html
    were interacting with a Maven plugin bug:
    
    [MJAVADOC-539] Upgrading plugin from 3.0.0 to 3.0.1 and 3.1.0 breaks certain external javadoc links
    
    https://issues.apache.org/jira/browse/MJAVADOC-539

--- book/src/main/java/com/pragmatickm/password/taglib/book/PragmaticKmPasswordTldInitializer.java
+++ book/src/main/java/com/pragmatickm/password/taglib/book/PragmaticKmPasswordTldInitializer.java
@@ -33,9 +33,9 @@ public class PragmaticKmPasswordTldInitializer extends TagReferenceInitializer {
 			"Taglib Reference",
 			"/password/taglib",
 			"/pragmatickm-password.tld",
-			Maven.properties.getProperty("javac.link.javaApi.jdk6"),
+			Maven.properties.getProperty("javac.link.javaApi.jdk7"),
 			Maven.properties.getProperty("javac.link.javaeeApi.6"),
-			Collections.singletonMap("com.pragmatickm.password.taglib.", Maven.properties.getProperty("documented.url") + "apidocs")
+			Collections.singletonMap("com.pragmatickm.password.taglib.", Maven.properties.getProperty("documented.url") + "apidocs/")
 		);
 	}
 }
