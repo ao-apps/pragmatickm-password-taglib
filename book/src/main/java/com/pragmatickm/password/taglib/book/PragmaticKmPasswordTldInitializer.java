3dce24a0ba985a438e416a9377d816a5f39e4359
AO Industries, Inc <support@aoindustries.com>
2020-05-29 19:00:04 -0500

    Trailing dot no longer required

--- book/src/main/java/com/pragmatickm/password/taglib/book/PragmaticKmPasswordTldInitializer.java
+++ book/src/main/java/com/pragmatickm/password/taglib/book/PragmaticKmPasswordTldInitializer.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-password-taglib - Passwords nested within SemanticCMS pages and elements in a JSP environment.
- * Copyright (C) 2017, 2019  AO Industries, Inc.
+ * Copyright (C) 2017, 2019, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -35,7 +35,8 @@ public class PragmaticKmPasswordTldInitializer extends TagReferenceInitializer {
 			"/pragmatickm-password.tld",
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
 			Maven.properties.getProperty("documented.javadoc.link.javaee"),
-			Collections.singletonMap("com.pragmatickm.password.taglib.", Maven.properties.getProperty("project.url") + "apidocs/")
+			// Self
+			Collections.singletonMap("com.pragmatickm.password.taglib", Maven.properties.getProperty("project.url") + "apidocs/")
 		);
 	}
 }
