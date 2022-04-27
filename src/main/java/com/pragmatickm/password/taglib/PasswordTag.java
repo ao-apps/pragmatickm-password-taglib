6f89e69cea0404fbdc4e044bb71b1bd966fb5725
AO Industries, Inc <support@aoindustries.com>
2022-04-27 00:14:53 -0500

    Performed automatic clean-up with org.openrewrite.java.cleanup.Cleanup

--- src/main/java/com/pragmatickm/password/taglib/PasswordTag.java
+++ src/main/java/com/pragmatickm/password/taglib/PasswordTag.java
@@ -117,7 +117,7 @@ public class PasswordTag extends ElementTag<Password> {
           htmlRenderer,
           pageIndex,
           new Document(serialization, doctype, characterEncoding, out)
-              .setAutonli(false) // Do not add extra newlines to JSP
+              .setAutonli(false)// Do not add extra newlines to JSP
               .setIndent(false), // Do not add extra indentation to JSP
           context,
           element
